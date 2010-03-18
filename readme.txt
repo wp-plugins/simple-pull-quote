387109471249e398da61f29bc845bce8b60001b7
llamaman <llamaman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-18 21:42:41 +0000

    Add version 1.0 files
    git-svn-id: https://plugins.svn.wordpress.org/simple-pull-quote/tags/1.0@219294 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== Simple Pull Quote ===
+Contributors: llamaman, themightymo
+Donate link: http://themightymo.com/donate
+Tags: pull quote,quotes, quotation
+Requires at least: 2.5
+Tested up to: 2.9.2
+Stable tag: trunk
+
+The Simple Pull Quote WordPress Plugin provides an easy way for you to insert pull quotes into your posts and pages.  
+
+== Description ==
+
+Simple Pull Quote Wordpress Plugin provides an easy way for you to insert pull quotes into your posts and pages.  It adds an easy-to-use "Pullquote" button to both the HTML and TinyMCE editors. 
+
+
+== Installation ==
+
+1. Either use the built-in Wordpress plugin installer to grab the plugin from the Wordpress plugin repository, or upload the entire contents of the `simple-pull-quote.zip` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+Q: What if I am using a visual editor besides TinyMCE?
+A: You will need to manually add the shortcode [pullquote]YOUR TEXT HERE[/pullquote]
+
+Q: Is there a maximum number of pull quotes I can have on a single blog post or page?
+A: No.  You can have as many pull quotes as you would like.
+
+Q: How do I change the colors and background image on the pullquotes?
+A: Override the default "simplePullQuote" class in your theme's CSS file.
+
+= Questions, Comments, Pizza Recipes? =
+
+Look me up on [Twitter](http://twitter.com/themightymo "Twitter") or contact me [here](http://www.themightymo.com/contact-us/ "Contact Me").
+
+
+== Usage ==
+
+1. Select the text that you want to use as your pull quote.
+2. Click on the "Pullquote" button in either the Visual or HTML editor.
+<br /><br />For more help on usage, visit the [Simple Pull Quote Homepage](http://themightymo.com/simple-pull-quote "Simple Pull Quote Wordpress Plugin") for a visual guide.
+
+= How do I update the look of the pull quotes? =
+
+To change the look of your pull quotes, open your theme's "style.css" file and create a CSS class called "simplePullQuotes".  Edit this class according to your tastes.  **IMPORTANT:** Make sure your theme's "wp_head()" function comes before your theme's stylesheet or else this won't work.
+
+= How do I use more than one pull quote in a single post or page? =
+
+Simply select the text that you want to use as a pull quote and click the "Pullquote" button in either the visual or html editor.
+
+== Upgrade Notice == 
+
+Version 1.0 of Simple Pull Quote is backwards compatible with previous versions.  Upgrade freely!
+
+== Changelog == 
+
+0.2 - Initial public release<br />
+0.2.1 - Removed text-based quotes by default. Now users must add their own quotes if they want them.  This gives users more flexibility.<br />
+0.2.2 - Added shortcode for multiple quotes: [quote1], [quote2]<br />
+0.2.3 - Switched the quote .gif file with a .png that will support any color background.<br />
+0.2.4 - Fixed Wordpress Plugin Repository file downloading issue.
+1.0 - No need for custom fields!  (However legacy support for custom fields still exists.)<br />
+	- Added "Pullquote" buttons to both the html and visual editors.<br />
+	- Updated CSS Code<br />
+	- Many thanks to [Darrell Schulte](http://twitter.com/darrell_schulte "Darrell Schulte") for his help in making version 1.0 a reality! 
+
+
+== Screenshots ==
+
+1. Simple Pull Quote plugin in action!
+2. Add pullquotes via the Wordpress TinyMCE editor.
+3. Add pullquotes via the Wordpress HTML editor.
---
+++ simple-pull-quote.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * @package Simple Pull Quote
+ * @author Toby Cryns
+ * @version 1.0
+ */
+/*
+Plugin Name: Simple Pull Quote
+Plugin URI: http://www.themightymo.com/simple-pull-quote
+Description: Easily add pull quotes to blog posts using shortcode.
+Author: Toby Cryns
+Version: 1.0
+Author URI: http://www.themightymo.com/updates
+*/
+
+/*  Copyright 2009  Toby Cryns  (email : toby at themightymo dot com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Load the TinyMCE Stuff
+require_once (dirname(__FILE__) . '/simple-pull-quote_tinymce.php');
+
+function my_css() {
+        echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') .'/wp-content/plugins/simple-pull-quote/css/simple-pull-quote.css" />' . "\n";
+}
+
+function getSimplePullQuote( $atts, $content = null ) {
+        /* Add CSS classes to the pull quote (a.k.a. Style the thing!) */
+        return '<div class="simplePullQuote">'.$content.'</div>';
+}
+
+// Allow us to add the pull quote using Wordpress shortcode, "[pullquote][/pullquote]" 
+add_shortcode('pullquote', 'getSimplePullQuote');
+
+// Add the CSS file to the header when the page loads
+add_action('wp_head', 'my_css');
+
+/* Call the javascript file that loads the html editor button */
+add_action('admin_print_scripts', 'simplePullQuotes');
+function simplePullQuotes() {
+	wp_enqueue_script(
+		'simple-pull-quotes',
+		plugin_dir_url(__FILE__) . 'simple-pull-quote.js'
+	);
+}
+
+// Load the custom TinyMCE plugin
+function simple_pull_quotes_plugin( $plugins ) {
+	$plugins['simplepullquotes'] = plugins_url('/simple-pull-quote/tinymce3/editor_plugin.js');
+	return $plugins;
+}
+
+
+
+
+// LEGACY CODE for Version < 0.2.4
+
+function getQuote(){
+	global $post;
+	$my_custom_field = get_post_meta($post->ID, "quote", true);
+	/* Add CSS classes to the pull quote (a.k.a. Style the thing!) */
+	return '<div class="simplePullQuote">'.$my_custom_field.'</div>'; 
+}
+
+/* Allow us to add the pull quote using Wordpress shortcode, "[quote]" */
+add_shortcode('quote', 'getQuote');
+function getQuote1(){
+	global $post;
+	$my_custom_field = get_post_meta($post->ID, "quote1", true);
+	/* Add CSS classes to the pull quote (a.k.a. Style the thing!) */
+	return '<div class="simplePullQuote">'.$my_custom_field.'</div>'; 
+}
+
+/* Allow us to add the pull quote using Wordpress shortcode, "[quote]" */
+add_shortcode('quote1', 'getQuote1');
+
+function getQuote2(){
+	global $post;
+	$my_custom_field = get_post_meta($post->ID, "quote2", true);
+
+	/* Add CSS classes to the pull quote (a.k.a. Style the thing!) */
+	return '<div class="simplePullQuote">'.$my_custom_field.'</div>'; 
+}
+
+// Allow us to add the pull quote using Wordpress shortcode, "[quote]" */
+add_shortcode('quote2', 'getQuote2');
+
+?>
